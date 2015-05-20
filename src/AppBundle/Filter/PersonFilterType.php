df5f44d8983e3578f3380e2183aadbac293336ad
Mieszko Malawski <miechuliv@tlen.pl>
2015-05-20 19:03:05 +0200

    filtry

---
+++ src/AppBundle/Filter/PersonFilterType.php
@@ -0,0 +1,29 @@
+<?php
+// ItemFilterType.php
+namespace Project\Bundle\SuperBundle\Filter;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ItemFilterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', 'filter_text');
+        $builder->add('rank', 'filter_number');
+    }
+
+    public function getName()
+    {
+        return 'item_filter';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'csrf_protection'   => false,
+            'validation_groups' => array('filtering') // avoid NotBlank() constraint-related message
+        ));
+    }
+}
